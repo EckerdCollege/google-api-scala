9e6266187810e793fcd44800b35d3aea41e10e07
Christopher Davenport <ChristopherDavenport@outlook.com>
2016-05-07 14:48:21 -0400

    Changed Implicit Conversions to Local, added  new Create with File

--- src/main/scala/google/services/drive/models/Permission.scala
+++ src/main/scala/google/services/drive/models/Permission.scala
@@ -1,23 +1,12 @@
 package google.services.drive.models
 
-import scala.language.implicitConversions
+
 /**
   * Created by davenpcm on 5/4/16.
   */
-case class Permission(emailAddress: String, role: String, permissionType: String, displayName: String)
-
-object Permission {
-
-  implicit def toGoogleApi(permission: Permission): com.google.api.services.drive.model.Permission = {
-    val Permission = new com.google.api.services.drive.model.Permission()
-      .setEmailAddress(permission.emailAddress)
-      .setRole(permission.role)
-      .setType(permission.permissionType)
-      .setDisplayName(permission.displayName)
-    Permission
-  }
-
-  implicit def fromGoogleApi(permission: com.google.api.services.drive.model.Permission): Permission = {
-    Permission(permission.getEmailAddress, permission.getRole, permission.getType, permission.getDisplayName)
-  }
-}
+case class Permission(emailAddress: String,
+                      role: String,
+                      permissionType: String,
+                      displayName: String ,
+                      id: Option[String] = None
+                     )
--- src/main/scala/google/services/drive/permissions.scala
+++ src/main/scala/google/services/drive/permissions.scala
@@ -1,22 +1,45 @@
 package google.services.drive
 
-
+import language.implicitConversions
+import language.postfixOps
 import models._
-import com.google.api.services.drive.model.PermissionList
-import scala.collection.JavaConverters._
+
 /**
   * Created by davenpcm on 5/4/16.
   */
 class permissions(drive: Drive) {
-  implicit val service = drive.drive
+  private val service = drive.drive
+
+  private implicit def toGoogleApi(permission: Permission): com.google.api.services.drive.model.Permission = {
+    val Permission = new com.google.api.services.drive.model.Permission()
+      .setEmailAddress(permission.emailAddress)
+      .setRole(permission.role)
+      .setType(permission.permissionType)
+      .setDisplayName(permission.displayName)
+    if (permission.id isDefined) { Permission.setId(permission.id.get)}
+    Permission
+  }
+
+  private implicit def fromGoogleApi(permission: com.google.api.services.drive.model.Permission): Permission = {
+    Permission(
+      permission.getEmailAddress,
+      permission.getRole,
+      permission.getType,
+      permission.getDisplayName,
+      Option(permission.getId)
+    )
+  }
 
   def list(fileId: String): List[Permission] = {
+    import com.google.api.services.drive.model.PermissionList
+    import scala.collection.JavaConverters._
+
     val result = service.permissions().list(fileId)
       .execute()
 
     val typedList = List[PermissionList](result)
       .map(permissions => permissions.getPermissions.asScala.toList)
-      .foldLeft(List[Permission]())((acc, listGroups) => listGroups.map(Permission.fromGoogleApi) ::: acc)
+      .foldLeft(List[Permission]())((acc, listGroups) => listGroups.map(fromGoogleApi) ::: acc)
 
     typedList
   }
@@ -27,6 +50,23 @@ class permissions(drive: Drive) {
 
   def delete(fileId: String, permissionId: String): Unit = {
     service.permissions().delete(fileId, permissionId).execute()
+  }
+
+  def create(
+            file: File,
+            permission: Permission,
+            sendNotificationEmail: Boolean,
+            emailMessage: String = "",
+            transferOwnership: Boolean = false): Permission = {
+    val initService = service.permissions().create(file.id.get, permission)
+      .setTransferOwnership(transferOwnership)
+      .setSendNotificationEmail(sendNotificationEmail)
+
+    sendNotificationEmail match {
+      case true => initService.setEmailMessage(emailMessage).execute()
+      case false => initService.execute()
+
+    }
   }
 
   def create(
